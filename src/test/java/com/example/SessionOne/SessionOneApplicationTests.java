832ebc6c96a97e81516e1f914b8ce1e9a9eb2c65
Meghna Bajoria <meghnabajoria@Meghnas-MacBook-Pro.local>
2021-02-22 11:01:07 +0530

    boiler plate

---
+++ src/test/java/com/example/SessionOne/SessionOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SessionOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
